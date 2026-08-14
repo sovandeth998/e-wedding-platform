952c571b302833e48867ac093bd71e9ca53e8da3
sovandeth998 <40328337+sovandeth998@users.noreply.github.com>
2026-08-14 17:27:22 +0700

    Fix syntax format in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-Set-Content -Path "next.config.mjs" -Value "/** @type {import('next').NextConfig} */
+@'
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -8,4 +9,5 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;" -Encoding UTF8
+export default nextConfig;
+'@ | Out-File -FilePath "next.config.mjs" -Encoding utf8
