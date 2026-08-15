b344dab44fbdba16f84ad3ec8e70377e9970fe20
sovandeth998 <40328337+sovandeth998@users.noreply.github.com>
2026-08-15 20:13:29 +0700

    Fix build errors, skip lib check, and resolve configs

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: "https",
+        hostname: "**",
       },
     ],
   },
